b5b959ab152c5f6d602070105e9ec17718e71333
Faellds <116304664+Faellds@users.noreply.github.com>
2022-11-16 14:32:32 -0300

    Add files via upload

---
+++ aula 1.py
@@ -0,0 +1,21 @@
+# Meu primeiro projeto python!!!
+#
+# print() = comando de saida
+print("Alo mundo!")
+
+# Quando quiser guardar um String!(frase)
+nome = "Rafael Ramos"
+# Quando eu quiser guardar um numero inteiro
+idade = 28
+
+# exibir o meu nome (que está dentro da variável nome)
+print(nome)
+
+#quando quiser exibir a frase "minha idade é" completando com o conteúdo da variável idade
+print("minha idade é "+str(idade))
+
+'''
+comentario em bloco
+
+'''
+
